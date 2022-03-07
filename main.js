d6738e6b0661d821fdf506e916fee85f45f4df22
mi2ne <show3070@gmail.com>
2022-03-08 07:14:50 +0900

    bootstrap #6
    최적화 테스트

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+import Dropdown from 'bootstrap/js/dist/dropdown' // npm i @popperjs/core 필요
+import Modal from 'bootstrap/js/dist/modal'
+
+const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+dropdownElementList.map(function (dropdownToggleEl) {
+  return new Dropdown(dropdownToggleEl)
+})
+
+// new Modal(document.getElementById('#exampleModal'), {
+//   backdrop: false,
+//   keyboard: false,
+//   focus: false
+// })
